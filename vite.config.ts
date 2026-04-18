609dcddbcb115c7d4953ffbcdc0d91f73e74df0f
Aaliyan Ahmed <a_aaliyan@yahoo.com>
2026-04-18 17:01:04 +0500

    FIXED TanStack Errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-  ],
-  base: "/",
+  plugins: [tanstackStart()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
