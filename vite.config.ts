db023e346bff075a9d8dbcd49dc2b278c79e6a88
Aaliyan Ahmed <a_aaliyan@yahoo.com>
2026-04-18 16:13:45 +0500

    Added vercel config json

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import react from "@vitejs/plugin-react";
 
-const basePath = process.env.BASE_PATH || "/lead-core";
+const basePath = process.env.BASE_PATH || "/";
 
 export default defineConfig({
     plugins: [react()],
